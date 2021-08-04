056c9aa64fcdadc283b973e699f6e69e5c7d486d
MASATAKE KAI <nerotan0806@gmail.com>
2021-08-04 18:37:24 +0900

    JS更新

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-  if($(window).scrollTop() > 1300) {
+  if($(window).scrollTop() > 1350) {
     $('#gnav_list').addClass('is-animated');
   } else {
     $('#gnav_list').removeClass('is-animated');
